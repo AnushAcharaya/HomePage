545d58c592cf72ee7f8e4378d53e48d8334bab79
Anush <np03cs4a230034@heraldcollege.edu.np>
2025-09-24 20:52:05 +0545

    making nav component

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import HomePage from './pages/HomePage'
+import Nav from "./components/Nav";
+
+function App() {
+
+
+  return (
+    <Router>
+      <Nav />
+
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+
+        />
+
+
+
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+
+import HeroSection from "../components/HeroSection";
+
+
+const HomePage = () => {
+    return (
+        <>
+            <HeroSection />
+        </>
+
+
+
+
+    );
+};
+export default HomePage;
